f403ea4ef511ef1fa031a4f6a494f462da3044b7
rChinnnn <b10121035@yuntech.edu.tw>
2024-01-16 18:26:01 +0800

    feat: add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import React from "react";
+import React from "react"
+import Landing from "./landing"
 
 export default function index() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-6xl font-bold text-slate-900">CrypTube Visions</h1>
-      <h3 className="text-2xl mt-8 text-slate-900">
-        Next.js, TypeScript, TailwindCSS, Solidity, IPFS, The Graph and Polygon
-      </h3>
-    </div>
-  );
+	return <Landing />
 }
